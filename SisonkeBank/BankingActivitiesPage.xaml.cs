98e20fe909d66befbda19a74d566b3982018e55a
Abi Adeyanju <faithade34@gmail.com>
2024-12-12 00:05:36 +0200

    Update BankingActivitiesPage.xaml.cs

--- SisonkeBank/BankingActivitiesPage.xaml.cs
+++ SisonkeBank/BankingActivitiesPage.xaml.cs
@@ -1,13 +1,4 @@
-﻿/* Name of the file: BankingActivitiesPage.xaml.cs
-*Name of the author: Abisola O Adeyanju
-*Date created: 19/06/2024
-*Operating system: Cross-platform
-*Version: 1.0
-*Description of the code: Code-behind for the banking activities page, displaying transaction history and details.
-*/
-
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using System;
 using SisonkeBank.Model; 
 using System.Collections.ObjectModel;
